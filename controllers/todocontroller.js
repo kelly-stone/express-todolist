c1fc9370ce0bdf297f72067b35eac56160715073
Kelly <kellyandstone@gmail.com>
2019-05-30 20:46:55 +1200

    using bootcdn to setup page

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,6 +1,8 @@
+var data = [{ item: "shopping" }, { item: "eating" }, { item: "coding" }];
+
 module.exports = function(app) {
   app.get("/todo", function(req, res) {
-    res.render("todo");
+    res.render("todo", { todos: data }); //todos from ejs
   });
 
   app.post("/todo", function(req, res) {});
